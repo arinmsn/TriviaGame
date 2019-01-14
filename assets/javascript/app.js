b3f7d55fc63f74d141571087726098daaeda311a
arinmsn <arinmsn@gmail.com>
2019-01-14 12:53:42 -0800

    Jumbotron fixed to screen / background color

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+// Create Global variables
+// Store answers 
+
+// Start the game
+
+// Listen to what user clicks on (which radio button aka answer)
+// Do not allow user to pick more than one answer per question!
+
+// Timer starts when START button is pressed
+// Check to see if time is up
+
+// Check Answers 
+// Need to track questions answered correctly, incorrectly, and missed (not answered)
+
+
+
+
